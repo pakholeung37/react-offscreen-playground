bb6b8fc40875f3803a833e60118c460fd990fcac
pakholeung37 <pakholeung37@gmail.com>
2024-01-15 14:44:52 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -26,27 +26,32 @@ function App() {
       >
         toggle
       </button>
-      <Activity mode={mode}>
-        <ABB countFormParent={count} />
-      </Activity>
+      <div style={{ display: 'flex ', gap: 40 }}>
+        <Activity mode={mode}>
+          <Inner countFormParent={count} from={'activity'} />
+        </Activity>
+        <div style={{ display: mode === 'visible' ? 'block' : 'none' }}>
+          <Inner countFormParent={count} from={'display'} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
 
-function ABB(props: { countFormParent: number }) {
+function Inner(props: { countFormParent: number, from: 'activity' | 'display' }) {
   const [count, setCount] = useState(0);
 
   React.useEffect(() => {
-    console.log("mount ABB");
+    console.log("mount Inner");
     return () => {
-      console.log("unmount ABB");
+      console.log("unmount Inner");
     };
   }, []);
 
   React.useMemo(() => {
-    console.log("memo ABB");
+    console.log("memo Inner");
   }, []);
 
   React.useEffect(() => {
@@ -63,7 +68,7 @@ function ABB(props: { countFormParent: number }) {
   }, []);
   return (
     <div>
-      <h2>I'am In Activity</h2>
+      <h2>I'am In {props.from}</h2>
       <div>counter from Parent{props.countFormParent}</div>
       <button onClick={add}>add</button>
       <div>counter inner{count}</div>
